feab6e32f71b4fc86295d495593b6e2a32f42b96
atlanfik11-coder <atlanfik11@gmail.com>
2026-09-04 10:50:58 +0300

    Add sitemap.xml, robots.txt, Google Search Console verification file and updated routes

--- api/robots.js
+++
@@ -1,7 +0,0 @@
-module.exports = (req, res) => {
-  res.setHeader('Content-Type', 'text/plain');
-  res.status(200).send(`User-agent: *
-Allow: /
-
-Sitemap: https://gameofpdf.com/sitemap.xml`);
-};
