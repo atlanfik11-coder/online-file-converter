5b635200e78863eae2474f3a28583cf374301f28
atlanfik11-coder <atlanfik11@gmail.com>
2026-09-04 10:43:38 +0300

    Add sitemap.xml, robots.txt, Google Search Console verification file and updated routes

---
+++ api/sitemap.js
@@ -0,0 +1,30 @@
+module.exports = (req, res) => {
+  res.setHeader('Content-Type', 'application/xml');
+  res.status(200).send(`<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://gameofpdf.com/</loc>
+    <lastmod>2026-09-04</lastmod>
+    <changefreq>daily</changefreq>
+    <priority>1.0</priority>
+  </url>
+  <url>
+    <loc>https://gameofpdf.com/converter</loc>
+    <lastmod>2026-09-04</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.9</priority>
+  </url>
+  <url>
+    <loc>https://gameofpdf.com/video</loc>
+    <lastmod>2026-09-04</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://gameofpdf.com/opket</loc>
+    <lastmod>2026-09-04</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.7</priority>
+  </url>
+</urlset>`);
+};
